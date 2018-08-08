f03308cde289371f75456315fb6d8596f07e3bba
ChathuraSam <chathura.samarajeewa@gmail.com>
2018-08-08 22:58:09 +0530

    Added to the local git repo

---
+++ js/myScript.js
@@ -0,0 +1,7 @@
+var loginForm = document.forms.form-group;
+
+var email = loginForm[1].elements.exampleInputEmail1.value;
+var password = loginForm[1].elements.exampleInputPassword1.value;
+
+console.log('Email : '+email);
+console.log('Password : '+password);
