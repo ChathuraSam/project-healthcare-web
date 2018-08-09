195dac57d0bbcb34e839f6a48a5795f477338cc1
ChathuraSam <chathura.samarajeewa@gmail.com>
2018-08-09 23:03:36 +0530

    Added new app.js file

--- js/myScript.js
+++ js/myScript.js
@@ -1,7 +1,25 @@
-var loginForm = document.forms.form-group;
+var loginForm = document.forms.form - group;
+var lblNavBarDate = document.getElementById('nav-bar-date');
+
+
+var date = new Date();
+var n = date.toDateString();
+
+var time = date.toLocaleTimeString();
+
+
+console.log('date:', n);
+
+console.log('time:', time);
+
+
 
 var email = loginForm[1].elements.exampleInputEmail1.value;
 var password = loginForm[1].elements.exampleInputPassword1.value;
 
-console.log('Email : '+email);
-console.log('Password : '+password);
+console.log('Email : ' + email);
+console.log('Password : ' + password);
+
+
+function getDateTime() {
+   
